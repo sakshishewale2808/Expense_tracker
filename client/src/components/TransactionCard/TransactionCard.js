5691004bdd5911bc552fffa4b451247cef4387b9
sakshishewale <sakshishewale2808@gmail.com>
2024-07-26 20:47:26 +0530

    Home and login added

---
+++ client/src/components/TransactionCard/TransactionCard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./TransactionCard.css"
+import axios from 'axios'
+import toast,{Toaster} from 'react-hot-toast'
+
+
+function TransactionCard({_id,title,amount,category,type,createdAt, loadTransactions }) {
+
+  const deleteTransaction = async () => {
+    const response = await axios.delete(`${process.env.REACT_APP_API_URI}/transaction/${_id}`)
+
+    toast.success(response.data.message)
+    loadTransactions ()
+   
+  }
+  return (
+    <div className='transaction-card'>
+      <h1 className='transaction-card-title'>{title}</h1>
+      <span className="transaction-card-date">
+        {new Date(createdAt).toLocaleString()}
+      </span>
+      <span className="transaction-card-category">
+        {category}
+      </span>
+      <span className='transaction-card-amount'style={{
+        color:type==="credit"?"green":"red"
+      }}>
+        {type==="credit"?"+":"-"}
+        {amount}
+      </span>
+      <button className="transaction-card-delete" onClick={deleteTransaction}>
+        Delete
+      </button>
+      <Toaster/>
+    </div>
+  )
+}
+
+export default TransactionCard
---
+++ client/src/views/Home/Home.js
@@ -0,0 +1,120 @@
+import React,{useEffect,useState} from 'react'
+import "./Home.css"
+import toast,{Toaster} from 'react-hot-toast'
+import axios from 'axios'
+import TransactionCard from './../../components/TransactionCard/TransactionCard'
+import ImgAdd from "./add.png"
+import { Link } from 'react-router-dom'
+
+function Home() {
+  const[user,setUser]=useState('')
+  const [transactions , setTransactions] = useState([])
+  const [netIncome, setNetIncome] = useState(0)
+  const [netExpense, setNetExpense] = useState(0)
+
+  useEffect(()=>{
+const currentUser =JSON.parse(localStorage.getItem('currentUser'))
+if(currentUser){
+  setUser(currentUser)
+}
+if(!currentUser){
+  window.location.href='/login'
+}
+  },[])
+  const loadTransactions=async()=>{
+    if(!user._id){
+      return
+    }
+    toast.loading('loding Transactions')
+const response = await axios.get(`${process.env.REACT_APP_API_URI}/transactions?userId=${user._id}`)
+const allTransactions=response.data.data
+toast.dismiss()
+
+
+setTransactions(allTransactions)
+  }
+  useEffect(()=>{
+    loadTransactions()
+  },[user])
+  useEffect(() => {
+    let income = 0
+    let expense = 0
+
+    transactions.forEach((transaction) => {
+      if (transaction.type === 'credit') {
+        income += transaction.amount
+      }
+      else{
+        expense += transaction.amount
+      }
+    })
+
+    setNetIncome(income)
+    setNetExpense(expense)
+  }, [transactions])
+  return (
+    <div>
+      <h1 className='home-greeting'>Hello  {user.fullName}</h1>
+      <h2 className='home-heading'>Welcome To Expense Tracker</h2>
+      <span className='home-logout'onClick={()=>{
+        localStorage.clear()
+        toast.success('logged successfully')
+        setTimeout(() => {
+          window.location.href = '/'
+        }, 3000)
+      }}>
+        Logout
+      </span>
+      <div className='net-transaction-value'>
+      <div className='net-transaction-value-item'>
+        <span className='net-transaction-value-amount'>
+          + {netIncome}
+          </span>
+          <span className='net-transaction-value-title'>
+           Net Income
+            </span>
+           </div>
+           <div className='net-transaction-value-item'>
+        <span className='net-transaction-value-amount'>
+          + {netExpense}
+          </span>
+          <span className='net-transaction-value-title'>
+            Net Expense
+            </span>
+           </div>
+           <div className='net-transaction-value-item'>
+        <span className='net-transaction-value-amount'>
+          {netIncome - netExpense}
+          </span>
+          <span className='net-transaction-value-title'>
+           Net Balance
+            </span>
+           </div>
+      </div>
+      <div className='transactions-con'>
+     {
+ transactions.map((transaction)=>{
+ const { _id,title,amount,category,type,createdAt}=transaction
+ return (<TransactionCard
+  key={_id}
+  _id={_id}
+  title={title}
+  amount={amount}
+  category={category}
+  type={type}
+  createdAt={createdAt}
+  loadTransactions={loadTransactions}
+  />)
+ })
+     }
+     </div>
+     <Link to='add-transaction' className='add-transaction-link'>
+     <img src={ ImgAdd} alt='add transaction' className='add-transaction'/>
+     </Link>
+      <Toaster/>
+    </div>
+  )
+}
+
+
+export default Home
